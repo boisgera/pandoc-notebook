82d6492a01f3119bab6aff473c4f70e7a0a4f326
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-06-22 16:40:27 +0200

    wip

--- build.py
+++ build.py
@@ -1,12 +1,40 @@
+#!/usr/bin/env python
+
+# Python Standard Library
 import copy
 import os.path
 import pathlib
 import tempfile
 
+# Pandoc
 import pandoc
 from pandoc.types import Div, CodeBlock
 
+# Plumbum
 from plumbum.cmd import jupyter
+
+
+# 🧠 Food for thought
+# ------------------------------------------------------------------------------
+
+# 🚧 TODO
+# ------------------------------------------------------------------------------
+#
+#   - Try a mkdocs material + mkdocsstrings setup to see how that flies
+
+
+# 🪲 BUGS
+# ------------------------------------------------------------------------------
+#
+#   - Jupyter nbconvert couldn't care less about image dpi, right?
+#     Mmm not true; at least when figsize and shape are set at figure creation
+#     time, the notebook cleared and then converted to HTML with nbconvert
+#     --execute we end up with size-dependent images. So what's the deal in
+#     our case ?
+#     OTOH: width and depth means NOTHING per se, dpi WILL fuck up the apparent
+#     size of the image. Getting a prescribed width and depth will need some
+#     work.
+#
 
 # TODO :
 #
@@ -22,10 +50,12 @@ from plumbum.cmd import jupyter
 #         import matplotlib_inline.backend_inline
 #         matplotlib_inline.backend_inline.set_matplotlib_formats("svg")
 #
-#   - 🪲 jupyter nbconvert couldn't care less about image dpi.
 
 
 def flag_code(doc):
+    """
+    Tag every CodeBlock in the doc with the class "code"
+    """
     for elt in pandoc.iter(doc):
         if isinstance(elt, CodeBlock):
             id, classes, kv = elt[0]
@@ -34,19 +64,23 @@ def flag_code(doc):
 
 
 def unflag_code(doc):
+    """
+    Remove the class "code" from every CodeBlock in the doc
+    """
     for elt in pandoc.iter(doc):
         if isinstance(elt, CodeBlock):
             id, classes, kv = elt[0]
             classes = list(set(classes) - {"code"})
             elt[0] = id, classes, kv
 
+
 def exec(doc, media="media"):
     tmp = pathlib.Path(tempfile.mkdtemp())
-    pandoc.write(doc, file=(tmp/"notebook.ipynb"), format="ipynb")
-    jupyter["nbconvert", "--to", "notebook", "--execute"](tmp / "notebook.ipynb")    
+    pandoc.write(doc, file=(tmp / "notebook.ipynb"), format="ipynb")
+    jupyter["nbconvert", "--to", "notebook", "--execute"](tmp / "notebook.ipynb")
     doc_ipynb = pandoc.read(
-        file=(tmp/"notebook.nbconvert.ipynb"), 
-        format="ipynb", 
+        file=(tmp / "notebook.nbconvert.ipynb"),
+        format="ipynb",
         options=["--extract-media", media],
     )
 
@@ -69,8 +103,11 @@ def exec(doc, media="media"):
     assert len(codeblocks_location) == len(outputs)
 
     for (holder, index), output in reversed(list(zip(codeblocks_location, outputs))):
-        holder[index+1:index+1] = output
+        holder[index + 1 : index + 1] = output
 
+
+# Main Entry Point
+# ------------------------------------------------------------------------------
 doc = pandoc.read(file="notebook.md")
 flag_code(doc)
 exec(doc)
--- notebook.md
+++ notebook.md
@@ -2,23 +2,28 @@
 % myself
 % today
 
-
---------------------------------------------------------------------------------
+---
 
 ```python
-import matplotlib as mpl
-print(mpl.rcParams['figure.dpi'])
-mpl.rcParams['figure.dpi'] = 300
-print(mpl.rcParams['figure.dpi'])
-mpl.rcParams['savefig.dpi'] = 300
+%matplotlib inline
+import matplotlib_inline
+matplotlib_inline.backend_inline.set_matplotlib_formats('svg')
 ```
 
-``` python
+```python
+#import matplotlib as mpl
+#print(mpl.rcParams['figure.dpi'])
+#mpl.rcParams['figure.dpi'] = 300
+#print(mpl.rcParams['figure.dpi'])
+#mpl.rcParams['savefig.dpi'] = 300
+```
+
+```python
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3])
 ```
 
-``` python
+```python
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3])
 ```
@@ -42,7 +47,7 @@ print("hello")
 
 ## Result
 
-``` python
+```python
 >>> 1 + 1
 ```
 
@@ -64,8 +69,7 @@ This image ![images](images/howtoexplain.jpg) will be included as a cell attachm
 
 ![images](images/howtoexplain.jpg)
 
-
---------------------------------------------------------------------------------
+---
 
 ```
 1 + 1
@@ -81,11 +85,70 @@ $$
 
 sljdksjdksdj $a=1$
 
-
 ```
 width = 345.0 / 72.27
 height = width / (16/9)
 print(width, height)
-plt.figure(figsize=(width, height))
-plt.plot([0, 2, 1, 0, 3])
+fig, axes = plt.subplots(figsize=(width, height))
+axes.plot([0, 2, 1, 0, 3, 3, 0, 0])
+```
+
+```
+width = 100 / 72 # target: 100 px. We assume a pixel = a dot. We convert that in inches.
+dpi = 72 # and we define a consistent dpi.
+height = width / (16/9)
+print(width, height)
+fig, axes = plt.subplots(figsize=(width, height), dpi=dpi)
+axes.plot([0, 2, 1, 0, 3, 3, 0, 0])
+```
+
+```
+width = 100
+dpi = 100 # and we define a consistent dpi.
+height = width / (16/9)
+print(width, height)
+fig, axes = plt.subplots(figsize=(width, height), dpi=dpi)
+axes.plot([0, 2, 1, 0, 3, 3, 0, 0])
+```
+
+```python
+width = 100
+height = 100
+dpi = 96 # my real dpi.
+print(width, height)
+fig, axes = plt.subplots(figsize=(width, height), dpi=dpi)
+axes.plot([0, 2, 1, 0, 3, 3, 0, 0])
+```
+
+```python
+width = 100
+height = 100
+dpi = 96 # my real (screen) dpi.
+print(width, height)
+fig, axes = plt.subplots(figsize=(width, height), dpi=dpi)
+axes.plot([-1,1, -1, 1, -1, 1, -1, 1])
+```
+
+```python
+width = height = 345 / 72.27
+dpi = 72 # matplotlib default ?
+print(width, height)
+fig, axes = plt.subplots(figsize=(width, height), dpi=dpi)
+axes.plot([-1,1, -1, 1, -1, 1, -1, 1])
+```
+
+```python
+width = height = 345 / 72.27
+dpi = 96 # my screen effective dpi
+print(width, height)
+fig, axes = plt.subplots(figsize=(width, height), dpi=dpi)
+axes.plot([-1,1, -1, 1, -1, 1, -1, 1])
+```
+
+```python
+width = height = 345 / 72.27
+dpi = 117.5 # my screen experimental dpi
+print(width, height)
+fig, axes = plt.subplots(figsize=(width, height), dpi=dpi)
+axes.plot([-1,1, -1, 1, -1, 1, -1, 1], color="red")
 ```
